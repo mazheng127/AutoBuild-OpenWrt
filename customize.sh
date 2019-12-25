ff91d64e393277100a61f78274c8d842fca85d6a
mazheng127 <mazheng127@gmail.com>
2019-12-25 12:01:57 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,3 +7,5 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+git clone https://github.com/tzxiaozhen88/luci-app-koolproxyR.git ./package/luci-app-koolproxyR
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git ./package/luci-app-adguardhome
